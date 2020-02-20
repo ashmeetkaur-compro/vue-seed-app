3f21056a162215ad280c76b12accfa515a382793
Ashmeet Kaur <ashmeet.kaur@comprotechnologies.com>
2020-02-20 18:26:41 +0530

    Initial commit

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template src="./HelloWorld.html"></template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style lang="scss" src="./HelloWorld.scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Vue Seed App"/>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld/HelloWorld.vue'
 
-export default {
+export default Vue.extend({
   name: 'Home',
   components: {
     HelloWorld
   }
-}
+})
 </script>
